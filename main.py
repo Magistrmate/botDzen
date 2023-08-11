b5e2fb1433b56392ca550a8ef1906652014b04ee
retro <retrok@yandex.ru>
2023-08-11 15:04:37 +0300

    create DB

--- main.py
+++ main.py
@@ -10,12 +10,38 @@ cursor = conn.cursor()
 async def main():
     async with app:
         async for member in app.get_chat_members(config.chat_dzen):
-            if member.user.is_self is False:
+            if member.user.is_self or member.user.is_bot is False:
                 user_id = member.user.id
+                is_contact = member.user.is_contact
+                is_mutual_contact = member.user.is_mutual_contact
+                is_deleted = member.user.is_deleted
+                is_verified = member.user.is_verified
+                is_restricted = member.user.is_restricted
+                is_scam = member.user.is_scam
+                is_fake = member.user.is_fake
+                is_premium = member.user.is_premium
+                first_name = member.user.first_name
+                last_name = member.user.last_name
+                status = member.user.status
+                last_online_date = member.user.last_online_date
+                next_offline_date = member.user.next_offline_date
                 username = member.user.username
+                language_code = member.user.language_code
+                emoji_status = member.user.emoji_status
+                dc_id = member.user.dc_id
+                phone_number = member.user.phone_number
+                photo = member.user.photo
+                restriction = member.user.restrictions
+                mention = member.user.mention
                 join_date = member.joined_date
-                cursor.execute('INSERT INTO users (user_id, username, join_date) VALUES (?,?,?)', (user_id,
-                                                                                                   username, join_date))
+                cursor.execute('INSERT INTO users (user_id, is_contact, is_mutual_contact, is_deleted, is_verified, '
+                               'is_restricted, is_scam, is_fake, is_premium, first_name, last_name, status, '
+                               'last_online_date, next_offline_date, username, language_code, emoji_status, dc_id, '
+                               'phone_number, photo, restriction, mention, join_date) VALUES (?,?,?)',
+                               (user_id, is_contact, is_mutual_contact, is_deleted, is_verified, is_restricted, is_scam,
+                                is_fake, is_premium, first_name, last_name, status, last_online_date, next_offline_date,
+                                username, language_code, emoji_status, dc_id, phone_number, photo, restriction, mention,
+                                join_date))
         conn.commit()
         # print(member.joined_date)
 
