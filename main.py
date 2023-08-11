0e71af4decba3cbe0f579206ace01d13bced4194
retro <retrok@yandex.ru>
2023-08-11 15:38:08 +0300

    create DB done

--- main.py
+++ main.py
@@ -22,27 +22,28 @@ async def main():
                 is_premium = member.user.is_premium
                 first_name = member.user.first_name
                 last_name = member.user.last_name
-                status = member.user.status
+                status = member.user.status.value
                 last_online_date = member.user.last_online_date
                 next_offline_date = member.user.next_offline_date
                 username = member.user.username
                 language_code = member.user.language_code
-                emoji_status = member.user.emoji_status
+                # emoji_status = member.user.emoji_status
                 dc_id = member.user.dc_id
                 phone_number = member.user.phone_number
-                photo = member.user.photo
+                # photo = member.user.photo.big_file_id
                 restriction = member.user.restrictions
                 mention = member.user.mention
                 join_date = member.joined_date
+                print(member)
                 cursor.execute('INSERT INTO users (user_id, is_contact, is_mutual_contact, is_deleted, is_verified, '
                                'is_restricted, is_scam, is_fake, is_premium, first_name, last_name, status, '
-                               'last_online_date, next_offline_date, username, language_code, emoji_status, dc_id, '
-                               'phone_number, photo, restriction, mention, join_date) VALUES (?,?,?)',
-                               (user_id, is_contact, is_mutual_contact, is_deleted, is_verified, is_restricted, is_scam,
-                                is_fake, is_premium, first_name, last_name, status, last_online_date, next_offline_date,
-                                username, language_code, emoji_status, dc_id, phone_number, photo, restriction, mention,
-                                join_date))
+                               'last_online_date, next_offline_date, username, language_code, dc_id, '
+                               'phone_number, restriction, mention, join_date) VALUES '
+                               '(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',
+                               (user_id, is_contact, is_mutual_contact, is_deleted, is_verified,
+                                is_restricted, is_scam, is_fake, is_premium, first_name, last_name, status,
+                                last_online_date, next_offline_date, username, language_code, dc_id,
+                                phone_number, restriction, mention, join_date))
         conn.commit()
-        # print(member.joined_date)
 
 app.run(main())
