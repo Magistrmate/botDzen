4e8b9d3c2a1e497097b62d0ac167fe6dafc1ca4a
retro <retrok@yandex.ru>
2023-08-28 15:50:11 +0300

    Process on messages

--- userbot.py
+++ userbot.py
@@ -17,127 +17,157 @@ def update_base(user_id, column_name, change_parameter, change_parameter_old):
 
 async def main():
     async with (app):
-        member_list = []
-        async for member in app.get_chat_members(config.chat_dzen):
-            member_list.append(member.user.id)
-            user_id = member.user.id
-            is_deleted = member.user.is_deleted
-            if is_deleted:
-                cursor.execute('INSERT INTO delete_users SELECT * FROM users WHERE user_id = ?',
-                               (user_id,))
-                cursor.execute('DELETE FROM users WHERE user_id = ?', (user_id,))
-            elif member.user.is_self or member.user.is_bot is False:
-                is_contact = member.user.is_contact
-                is_mutual_contact = member.user.is_mutual_contact
-                is_verified = member.user.is_verified
-                is_restricted = member.user.is_restricted
-                is_scam = member.user.is_scam
-                is_fake = member.user.is_fake
-                is_premium = member.user.is_premium
-                first_name = member.user.first_name
-                last_name = member.user.last_name
-                status = member.user.status.value
-                last_online_date = str(member.user.last_online_date)
-                next_offline_date = str(member.user.next_offline_date)
-                username = member.user.username
-                language_code = member.user.language_code
-                dc_id = member.user.dc_id
-                phone_number = member.user.phone_number
-                photo = member.user.photo
-                if photo is not None:
-                    photo = member.user.photo.big_file_id
-                restriction = member.user.restrictions
-                mention = member.user.mention
-                join_date = str(member.joined_date)
-                result = cursor.execute('SELECT * FROM users WHERE user_id = ?', (user_id,))
-                result_l = result.fetchone()
-                if result_l is not None:
-                    first_name_old = result_l[2]
-                    last_name_old = result_l[3]
-                    username_old = result_l[4]
-                    join_date_old = result_l[5]
-                    is_contact_old = result_l[6]
-                    is_mutual_contact_old = result_l[7]
-                    is_deleted_old = result_l[8]
-                    is_verified_old = result_l[9]
-                    is_restricted_old = result_l[10]
-                    is_scam_old = result_l[11]
-                    is_fake_old = result_l[12]
-                    is_premium_old = result_l[13]
-                    status_old = result_l[14]
-                    last_online_date_old = result_l[15]
-                    next_offline_date_old = result_l[16]
-                    language_code_old = result_l[17]
-                    dc_id_old = result_l[18]
-                    phone_number_old = result_l[19]
-                    photo_old = result_l[20]
-                    restriction_old = result_l[21]
-                    mention_old = result_l[22]
-                    if first_name != first_name_old:
-                        update_base(user_id, 'first_name', first_name, first_name_old)
-                    if last_name != last_name_old:
-                        update_base(user_id, 'last_name', last_name, last_name_old)
-                    if username != username_old:
-                        update_base(user_id, 'username', username, username_old)
-                    if join_date != join_date_old:
-                        update_base(user_id, 'join_date', join_date, join_date_old)
-                    if is_contact != is_contact_old:
-                        update_base(user_id, 'is_contact', is_contact, is_contact_old)
-                    if is_mutual_contact != is_mutual_contact_old:
-                        update_base(user_id, 'is_mutual_contact', is_mutual_contact, is_mutual_contact_old)
-                    if is_deleted != is_deleted_old:
-                        update_base(user_id, 'is_deleted', is_deleted, is_deleted_old)
-                    if is_verified != is_verified_old:
-                        update_base(user_id, 'is_verified', is_verified, is_verified_old)
-                    if is_restricted != is_restricted_old:
-                        update_base(user_id, 'is_restricted', is_restricted, is_restricted_old)
-                    if is_scam != is_scam_old:
-                        update_base(user_id, 'is_scam', is_scam, is_scam_old)
-                    if is_fake != is_fake_old:
-                        update_base(user_id, 'is_fake', is_fake, is_fake_old)
-                    if is_premium != is_premium_old:
-                        update_base(user_id, 'is_premium', is_premium, is_premium_old)
-                    if status != status_old:
-                        update_base(user_id, 'status', status, status_old)
-                    if last_online_date != last_online_date_old and (last_online_date
-                                                                     and last_online_date_old) != 'None':
-                        update_base(user_id, 'last_online_date', last_online_date, last_online_date_old)
-                    if next_offline_date != next_offline_date_old and (next_offline_date and
-                                                                       next_offline_date_old) != 'None':
-                        update_base(user_id, 'next_offline_date', next_offline_date, next_offline_date_old)
-                    if language_code != language_code_old:
-                        update_base(user_id, 'language_code', language_code, language_code_old)
-                    if dc_id != dc_id_old:
-                        update_base(user_id, 'dc_id', dc_id, dc_id_old)
-                    if phone_number != phone_number_old:
-                        update_base(user_id, 'phone_number', phone_number, phone_number_old)
-                    if photo != photo_old:
-                        update_base(user_id, 'photo', photo, photo_old)
-                    if restriction != restriction_old:
-                        update_base(user_id, 'restriction', restriction, restriction_old)
-                    if mention != mention_old:
-                        update_base(user_id, 'mention', mention, mention_old)
-                else:
-                    cursor.execute('INSERT INTO users (user_id, is_contact, is_mutual_contact, is_deleted, '
-                                   'is_verified, is_restricted, is_scam, is_fake, is_premium, first_name, last_name, '
-                                   'status, last_online_date, next_offline_date, username, language_code, dc_id, '
-                                   'phone_number, photo, restriction, mention, join_date) VALUES '
-                                   '(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',
-                                   (user_id, is_contact, is_mutual_contact, is_deleted, is_verified,
-                                    is_restricted, is_scam, is_fake, is_premium, first_name, last_name, status,
-                                    last_online_date, next_offline_date, username, language_code, dc_id,
-                                    phone_number, photo, restriction, mention, join_date))
-    cursor.execute('SELECT user_id FROM users')
-    result = cursor.fetchall()
-    for user_id in result:
-        member_user = False
-        for member in member_list:
-            if user_id[0] == member: #khj
-                member_user = True
-        if member_user is False:
-            cursor.execute('INSERT INTO leave_users SELECT * FROM users WHERE user_id = ?',
-                           (user_id[0],))
-            cursor.execute('DELETE FROM users WHERE user_id = ?', (user_id[0],))
+        # member_list = []
+        # async for member in app.get_chat_members(config.chat_dzen):
+        #     member_list.append(member.user.id)
+        #     user_id = member.user.id
+        #     is_deleted = member.user.is_deleted
+        #     if is_deleted:
+        #         cursor.execute('INSERT INTO delete_users SELECT * FROM users WHERE user_id = ?',
+        #                        (user_id,))
+        #         cursor.execute('DELETE FROM users WHERE user_id = ?', (user_id,))
+        #     elif member.user.is_self or member.user.is_bot is False:
+        #         is_contact = member.user.is_contact
+        #         is_mutual_contact = member.user.is_mutual_contact
+        #         is_verified = member.user.is_verified
+        #         is_restricted = member.user.is_restricted
+        #         is_scam = member.user.is_scam
+        #         is_fake = member.user.is_fake
+        #         is_premium = member.user.is_premium
+        #         first_name = member.user.first_name
+        #         last_name = member.user.last_name
+        #         status = member.user.status.value
+        #         last_online_date = str(member.user.last_online_date)
+        #         next_offline_date = str(member.user.next_offline_date)
+        #         username = member.user.username
+        #         language_code = member.user.language_code
+        #         dc_id = member.user.dc_id
+        #         phone_number = member.user.phone_number
+        #         photo = member.user.photo
+        #         if photo is not None:
+        #             photo = member.user.photo.big_file_id
+        #         restriction = member.user.restrictions
+        #         mention = member.user.mention
+        #         join_date = str(member.joined_date)
+        #         result = cursor.execute('SELECT * FROM users WHERE user_id = ?', (user_id,))
+        #         result_l = result.fetchone()
+        #         if result_l is not None:
+        #             first_name_old = result_l[2]
+        #             last_name_old = result_l[3]
+        #             username_old = result_l[4]
+        #             join_date_old = result_l[5]
+        #             is_contact_old = result_l[6]
+        #             is_mutual_contact_old = result_l[7]
+        #             is_deleted_old = result_l[8]
+        #             is_verified_old = result_l[9]
+        #             is_restricted_old = result_l[10]
+        #             is_scam_old = result_l[11]
+        #             is_fake_old = result_l[12]
+        #             is_premium_old = result_l[13]
+        #             status_old = result_l[14]
+        #             last_online_date_old = result_l[15]
+        #             next_offline_date_old = result_l[16]
+        #             language_code_old = result_l[17]
+        #             dc_id_old = result_l[18]
+        #             phone_number_old = result_l[19]
+        #             photo_old = result_l[20]
+        #             restriction_old = result_l[21]
+        #             mention_old = result_l[22]
+        #             if first_name != first_name_old:
+        #                 update_base(user_id, 'first_name', first_name, first_name_old)
+        #             if last_name != last_name_old:
+        #                 update_base(user_id, 'last_name', last_name, last_name_old)
+        #             if username != username_old:
+        #                 update_base(user_id, 'username', username, username_old)
+        #             if join_date != join_date_old:
+        #                 update_base(user_id, 'join_date', join_date, join_date_old)
+        #             if is_contact != is_contact_old:
+        #                 update_base(user_id, 'is_contact', is_contact, is_contact_old)
+        #             if is_mutual_contact != is_mutual_contact_old:
+        #                 update_base(user_id, 'is_mutual_contact', is_mutual_contact, is_mutual_contact_old)
+        #             if is_deleted != is_deleted_old:
+        #                 update_base(user_id, 'is_deleted', is_deleted, is_deleted_old)
+        #             if is_verified != is_verified_old:
+        #                 update_base(user_id, 'is_verified', is_verified, is_verified_old)
+        #             if is_restricted != is_restricted_old:
+        #                 update_base(user_id, 'is_restricted', is_restricted, is_restricted_old)
+        #             if is_scam != is_scam_old:
+        #                 update_base(user_id, 'is_scam', is_scam, is_scam_old)
+        #             if is_fake != is_fake_old:
+        #                 update_base(user_id, 'is_fake', is_fake, is_fake_old)
+        #             if is_premium != is_premium_old:
+        #                 update_base(user_id, 'is_premium', is_premium, is_premium_old)
+        #             if status != status_old:
+        #                 update_base(user_id, 'status', status, status_old)
+        #             if last_online_date != last_online_date_old and (last_online_date
+        #                                                              and last_online_date_old) != 'None':
+        #                 update_base(user_id, 'last_online_date', last_online_date, last_online_date_old)
+        #             if next_offline_date != next_offline_date_old and (next_offline_date and
+        #                                                                next_offline_date_old) != 'None':
+        #                 update_base(user_id, 'next_offline_date', next_offline_date, next_offline_date_old)
+        #             if language_code != language_code_old:
+        #                 update_base(user_id, 'language_code', language_code, language_code_old)
+        #             if dc_id != dc_id_old:
+        #                 update_base(user_id, 'dc_id', dc_id, dc_id_old)
+        #             if phone_number != phone_number_old:
+        #                 update_base(user_id, 'phone_number', phone_number, phone_number_old)
+        #             if photo != photo_old:
+        #                 update_base(user_id, 'photo', photo, photo_old)
+        #             if restriction != restriction_old:
+        #                 update_base(user_id, 'restriction', restriction, restriction_old)
+        #             if mention != mention_old:
+        #                 update_base(user_id, 'mention', mention, mention_old)
+        #         else:
+        #             cursor.execute('INSERT INTO users (user_id, is_contact, is_mutual_contact, is_deleted, '
+        #                            'is_verified, is_restricted, is_scam, is_fake, is_premium, first_name, last_name, '
+        #                            'status, last_online_date, next_offline_date, username, language_code, dc_id, '
+        #                            'phone_number, photo, restriction, mention, join_date) VALUES '
+        #                            '(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',
+        #                            (user_id, is_contact, is_mutual_contact, is_deleted, is_verified,
+        #                             is_restricted, is_scam, is_fake, is_premium, first_name, last_name, status,
+        #                             last_online_date, next_offline_date, username, language_code, dc_id,
+        #                             phone_number, photo, restriction, mention, join_date))
+        async for message in app.get_chat_history(config.chat_dzen):
+            # print(message.from_user.first_name, message.from_user.id, message.date, message.reply_to_message_id,
+            #       message.text)
+            if message.from_user is None:
+                user_id = 0
+            else:
+                user_id = message.from_user.id
+            if message.reply_to_top_message_id is None:
+                theme_id = message.reply_to_message_id
+            else:
+                theme_id = message.reply_to_top_message_id
+            if theme_id == 800:
+                theme_name = 'Помощь коллеге'
+            elif theme_id == 226:
+                theme_name = 'Просьбы о помощи'
+            elif theme_id == 1:
+                theme_name = 'Уведомления'
+            elif theme_id == 9:
+                theme_name = 'Отчеты о проделанной работе'
+            elif theme_id == 6:
+                theme_name = 'Светские беседы'
+            elif theme_id == 4:
+                theme_name = 'Ссылки на каналы'
+            else:
+                theme_name = 0
+            message_text = message.text
+            date = message.date
+            cursor.execute('INSERT INTO messages (user_id, theme_name, message_text, date) VALUES '
+                           '(?,?,?,?)', (user_id, theme_name, message_text, date,))
+    # cursor.execute('SELECT user_id FROM users')
+    # result = cursor.fetchall()
+    # for user_id in result:
+    #     member_user = False
+    #     for member in member_list:
+    #         if user_id[0] == member:
+    #             member_user = True
+    #     if member_user is False:
+    #         cursor.execute('INSERT INTO leave_users SELECT * FROM users WHERE user_id = ?',
+    #                        (user_id[0],))
+    #         cursor.execute('DELETE FROM users WHERE user_id = ?', (user_id[0],))
     conn.commit()
+
 
 app.run(main())
